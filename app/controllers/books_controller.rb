01bd77cd74be4a972139cd75143c9e504b5adaba
akihito <akihito2@MacBook-Pro-3.local>
2020-02-18 14:17:29 +0900

    [Fix] index & new

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   def index
-  	@book = Book.all
+  	@books = Book.all
   end
 
   def show
@@ -26,7 +26,7 @@ class BooksController < ApplicationController
   	redirect_to book_path(book)
   end
 
-  def dstroy
+  def destroy
   	book = Book.find(params[:id])
   	book.destroy
   	redirect_to books_path
